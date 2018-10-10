b22404bce5745763295ffd4708367bee590932b2
Daniel Quintero Bernal <daquinber@gmail.com>
2018-10-10 13:32:39 +0200

    Cambio de escena aún no va del todo guay. Gamemanager del tutorial no parece cargar cuando se trae de otra escena

---
+++ Assets/DontDestroyOnLoad.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Este componente permite que el objeto se mantenga en cambios de escenas
+/// </summary>
+public class DontDestroyOnLoad : MonoBehaviour {
+
+	void Start () {
+        DontDestroyOnLoad(this.gameObject);
+	}
+
+}
---
+++ Assets/TestButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TestButton : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("tutorial");
+    }
+
+}
